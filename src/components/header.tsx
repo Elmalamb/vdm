f3720df57da3779410dd2e1b52d5e78fff1007c3
Eli Mlmb <elmalamba@gmail.com>
2025-07-16 09:32:28 +0000

    La couleur du logo dois être en rouge.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -121,9 +121,9 @@ export function Header() {
 
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center bg-white text-zinc-900 border-b">
-      <Link href="/" className="flex items-center justify-center gap-2 text-primary" prefetch={false}>
-        <span className="font-bold">VenteDémo</span>
-        <Handshake className="h-6 w-6" />
+      <Link href="/" className="flex items-center justify-center gap-2" prefetch={false}>
+        <span className="font-bold text-primary">VenteDémo</span>
+        <Handshake className="h-6 w-6 text-destructive" />
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
         {!loading && (

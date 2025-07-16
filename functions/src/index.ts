90a32e71c32e923ca28edd7c4a8f8dcf646e79fb
Eli Mlmb <elmalamba@gmail.com>
2025-07-16 16:53:15 +0000

    Resultat

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,7 @@
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
-import {onUserCreate} from "firebase-functions/v2/auth";
+import {onUserCreate, UserRecord} from "firebase-functions/v2/auth";
 
 
 admin.initializeApp();
@@ -19,7 +19,7 @@ const db = admin.firestore();
 const storage = admin.storage();
 
 export const createUserDocument = onUserCreate(async (event) => {
-  const user = event.data;
+  const user: UserRecord = event.data;
   const {uid, email} = user;
 
   const userDocRef = db.collection("users").doc(uid);
